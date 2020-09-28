da2215ce87c10d7eb5ea270d1861c4098abe4e76
AMIT GUPTA <67703308+amitgit2020@users.noreply.github.com>
2020-09-28 22:12:57 +0530

    register user
    here i made an user basic registration form.

--- wisewools/views.py
+++ wisewools/views.py
@@ -1,12 +1,20 @@
 # This file is created by Amit 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account has been created for {username}!')
+            return redirect('home')
+    else:
+        form = UserCreationForm()
     return render(request, 'wisewools/signup.html',{"form": form})
 
 
--- woolsapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WoolsappConfig(AppConfig):
-    name = 'woolsapp'
