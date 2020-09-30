11a9e3eafc9def5f6572dee164f4838e0792b74e
AMIT GUPTA <67703308+amitgit2020@users.noreply.github.com>
2020-09-30 08:46:10 +0530

    Email field added
    new field added by using django forms

--- wisewools/views.py
+++ wisewools/views.py
@@ -1,48 +1,54 @@
-# This file is created by Amit 
+# This file is created by Amit
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
 from django.contrib import messages
 
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account has been created for {username}!')
+            messages.success(
+                request, f'Account has been created for {username}!')
             return redirect('home')
     else:
-        form = UserCreationForm()
-    return render(request, 'wisewools/signup.html',{"form": form})
+        form = UserRegisterForm()
+    return render(request, 'wisewools/signup.html', {"form": form})
 
 
 def login(request):
     # return HttpResponse('This is my login page.')
     return render(request, 'wisewools/login.html')
 
+
 def home(request):
     # return HttpResponse("This is my Home page")
     return render(request, 'wisewools/home.html')
+
 
 def art(request):
     # return HttpResponse("This is my Art page")
     return render(request, 'wisewools/art.html')
 
+
 def culture(request):
     # return HttpResponse("This is my Culture page")
     return render(request, 'wisewools/culture.html')
+
 
 def cinema(request):
     # return HttpResponse("This is my Cinema page")
     return render(request, 'wisewools/cinema.html')
 
+
 def photoessay(request):
     # return HttpResponse("This is my Photo Essay page")
     return render(request, 'wisewools/photoessay.html')
 
+
 def design(request):
     # return HttpResponse("This is my design page")
     return render(request, 'wisewools/design.html')
-
